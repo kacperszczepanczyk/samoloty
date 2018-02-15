9644e3ce7e5fa868bc49f938e4b06805dc1dbc59
Kacper Szczepańczyk <kacper.szczepanczyk@gmail.com>
2018-02-15 22:04:42 +0100

    change

--- Samoloty/src/Main.java
+++ Samoloty/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Mapa mapa = new Mapa(1000,1000);
 		ListaLotnisk listaLotnisk = new ListaLotnisk(3, mapa);
-		ListaSamolotow listaSamolotow = new ListaSamolotow(30, mapa, listaLotnisk);
+		ListaSamolotow listaSamolotow = new ListaSamolotow(30, mapa, listaLotnisk); // zmiana
 		new SwingAnimation(mapa, listaLotnisk, listaSamolotow);
 	}
 }
